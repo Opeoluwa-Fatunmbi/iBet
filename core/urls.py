b55e8982878471ed1c6ca5f0e1b56ca59acedcac
Opeoluwa Fatunmbi <opeoluwafatunmbi@gmail.com>
2023-08-01 01:26:07 +0100

    [Fix] Changed django-rest-auth to dj-rest-auth

--- core/urls.py
+++ core/urls.py
@@ -1,25 +1,31 @@
 from django.urls import path
-from .views import *
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework_simplejwt.views import TokenVerifyView
+from dj_rest_auth.views import (
+    LoginView,
+    LogoutView,
+    PasswordResetView,
+    PasswordResetConfirmView,
+    PasswordChangeView,
+    #VerifyEmailView,
+    #ResendEmailVerificationView,
 )
-from rest_framework_simplejwt.views import TokenBlacklistView
+
+app_name = 'core'
 
 urlpatterns = [
-    
+    # dj-rest-auth URLs
+    path('rest-auth/login/', LoginView.as_view(), name='rest_login'),
+    path('rest-auth/logout/', LogoutView.as_view(), name='rest_logout'),
+    path('rest-auth/password/reset/', PasswordResetView.as_view(), name='rest_password_reset'),
+    path('rest-auth/password/reset/confirm/', PasswordResetConfirmView.as_view(), name='rest_password_reset_confirm'),
+    path('rest-auth/password/change/', PasswordChangeView.as_view(), name='rest_password_change'),
+    #path('rest-auth/registration/', RegisterView.as_view(), name='rest_register'),
+    #path('rest-auth/registration/verify-email/', VerifyEmailView.as_view(), name='rest_verify_email'),
+    #path('rest-auth/registration/resend-verification-email/', ResendEmailVerificationView.as_view(), name='rest_resend_verification_email'),
 
-
-
-
-
-    #JWT
+    # JWT
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/token/blacklist/', TokenBlacklistView.as_view(), name='token_blacklist'),
-    
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
-
-
-
-
--- iBet/urls.py
+++ iBet/urls.py
@@ -10,8 +10,8 @@ schema_view = get_schema_view(
         default_version='v1',
         description="iBet",
         terms_of_service="https://www.example.com/terms/",
-        contact=openapi.Contact(email="contact@example.com"),
-        license=openapi.License(name="Your License"),
+        contact=openapi.Contact(email="opeoluwafatunmbi@gmail.com"),
+        license=openapi.License(name="MIT License"),
     ),
     public=True,
     permission_classes=(permissions.AllowAny,),
@@ -20,6 +20,10 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')), 
+    path('accounts/', include('allauth.urls')),
+    path('dj-rest-auth/', include('dj_rest_auth.urls')),
+    path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
+    #path('rest-auth/', include('rest_auth.urls')),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
