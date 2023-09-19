ae3d8b55256d8de916b15d5edf20f8ceb38e5b7e
Opeoluwa Fatunmbi <opeoluwafatunmbi@gmail.com>
2023-09-19 15:17:58 +0100

    [Update] The Signup endpoint under construction

--- apps/user/views.py
+++ apps/user/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from apps.user.serializers import SignupSerializer
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+
+
+class Signup(APIView):
+
+    serializer_class = SignupSerializer
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
--- iBet/settings/base.py
+++ iBet/settings/base.py
@@ -99,7 +99,6 @@ DATABASES = {
 
 AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
-    #'allauth.account.auth_backends.AuthenticationBackend',
 ]
 
 SITE_ID = 1
