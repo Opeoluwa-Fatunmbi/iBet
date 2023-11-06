78bcebfc043aaf9ab8c5e014881e0cc9a88dce92
Opeoluwa Fatunmbi <opeoluwafatunmbi@gmail.com>
2023-11-06 22:25:05 +0100

    [Add] Serializer Optimization(Match)

--- apps/betting/serializers.py
+++ apps/betting/serializers.py
@@ -1,12 +1,21 @@
 from rest_framework import serializers
 from apps.betting.models import Match, Bet, Outcome
+from django.contrib.auth.models import User
 
 
-class MatchSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Match
-        fields = "__all__"
 
+class MatchSerializer(serializers.Serializer):
+    match_date = serializers.DateTimeField()
+    game_type = serializers.CharField(max_length=100)
+    location = serializers.CharField(max_length=100)
+    status = serializers.CharField(max_length=20)
+    winner = serializers.PrimaryKeyRelatedField(queryset=User.objects.all(), required=False, allow_null=True)
+    loser = serializers.PrimaryKeyRelatedField(queryset=User.objects.all(), required=False, allow_null=True)
+    duration_minutes = serializers.IntegerField(required=False, allow_null=True)
+    notes = serializers.CharField(required=False)
+
+    def create(self, validated_data):
+        return Match.objects.create(**validated_data)
 
 class BetSerializer(serializers.ModelSerializer):
     class Meta:
